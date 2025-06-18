6dce794a1a0f6d0a8d86fa94d05fd94d3fab4bd8
aidankennedy47 <aidankennedy47@gmail.com>
2025-06-18 07:20:38 +0000

    [automatic commit] 2025-06-18 07:20:38 UTC 122.150.230.211

--- part1/routes/message.js
+++ part1/routes/message.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var db = require('../db');
 
-router.get('/items', async (req, res) => {
-
+router.get('/select', async (req, res) => {
+db.query()
 
 });
