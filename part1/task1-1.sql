33a910a6abee966428b3507f9291ce7ecb7192bd
aidankennedy47 <aidankennedy47@gmail.com>
2025-06-18 04:51:28 +0000

    [automatic commit] 2025-06-18 04:51:28 UTC 122.150.230.121

--- part1/task1-1.sql
+++ part1/task1-1.sql
@@ -55,4 +55,4 @@ INSERT INTO Location(City, State, Country)
 VALUES (Adelaide, SA, Australia);
 
 INSERT INTO User (name, email, password)
-VALUES ('Aidan', 'aidankennedy47@gmail.com')
+VALUES ('Aidan', 'aidankennedy47@gmail.com', 'secretpassword');
