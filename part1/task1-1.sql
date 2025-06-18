347977b6eadb2baa0d3cd6dc2147dd2ba59cf7df
aidankennedy47 <aidankennedy47@gmail.com>
2025-06-18 04:20:49 +0000

    [automatic commit] 2025-06-18 04:20:48 UTC 122.150.230.180

--- part1/task1-1.sql
+++ part1/task1-1.sql
@@ -7,10 +7,10 @@ UserID int NOT NULL AUTO_INCREMENT,
 `name` VARCHAR(100) NOT NULL,
 email VARCHAR(100) NOT NULL,
 `password` VARCHAR(100) NOT NULL,
-location_id int DEFAULT NULL,
+locationID int DEFAULT NULL,
 PRIMARY KEY (UserID),
 UNIQUE KEY (email),
-FOREIGN KEY (location_id) REFERENCES Locations(Location)
+FOREIGN KEY (locationID) REFERENCES Locations(LocationID)
 );
 
 
